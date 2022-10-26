69ee13e8bebd31499bd43861c59ec94d1261cc08
parag <paragjain528@gmail.com>
2022-10-26 14:03:33 +0530

    expenses details passed to ExpenseItem1 component using props

--- src/components/ExpenseItem1.js
+++ src/components/ExpenseItem1.js
@@ -1,20 +1,16 @@
-import './ExpenseItem.css';
-function ExpenseItem1(){
-
-
-    const expenseDate = new Date(2021,2,28);
-    const expenseTitle = 'car insurance';
-    const expenseAmount = 294.65;
-    const expenseLocation = 'Delhi';
+ import './ExpenseItem.css';
+function ExpenseItem1(props){
     return (
-        <div className= 'expense-item'>
-            <div>{expenseDate.toISOString()}</div>
-            <div className='expense-item__description'>
-                 <h2>{expenseTitle}</h2>
-                 <h2>{expenseLocation}</h2>
-                 <div className='expense-item__price'>${expenseAmount}</div>
-            </div>
-           </div>
+      
+        <div className='expense-item'>
+        <div>{props.date.toISOString()}</div>
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <h2>{props.location}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+        </div>
+  
     )
 }
 export default ExpenseItem1;
