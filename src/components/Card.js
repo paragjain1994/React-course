44b40a8d2689857132479de63fb4e9476fca2782
parag <paragjain528@gmail.com>
2022-10-26 17:56:21 +0530

    Card component with props.children property

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+// invoked from ExpenseItem1 component
+
+import './Card.css';
+function Card(props) {  
+  const classes = 'card ' + props.className;
+  console.log("classes contains"+ classes);
+  
+  return (
+  <div className={classes}>{props.children}</div>
+  );
+}
+
+export default Card;
--- src/components/ExpenseItem1.js
+++ src/components/ExpenseItem1.js
@@ -1,13 +1,14 @@
+ import Card from './Card';
  import ExpenseDate from './ExpenseDate';
  import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 function ExpenseItem1(props){
     return (
       
-        <div className='expense-item'>
+        <Card className='expense-item'>
         <ExpenseDate date={props.date}></ExpenseDate>
         <ExpenseDetails title={props.title} amount={props.amount} location={props.location}></ExpenseDetails>
-        </div>
+        </Card>
   
     )
 }
