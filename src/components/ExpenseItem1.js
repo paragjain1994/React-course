2fc099dfa76cfca9746058bc2081b2699c614c0e
parag <paragjain528@gmail.com>
2022-10-27 14:40:58 +0530

    added clickHandler to update amount of expense

--- src/components/ExpenseItem1.js
+++ src/components/ExpenseItem1.js
@@ -1,24 +1,34 @@
+ // invoked from Expenses.js component
  import Card from './Card';
  import ExpenseDate from './ExpenseDate';
  import ExpenseDetails from './ExpenseDetails';
  import './ExpenseItem.css';
  import React , {useState} from 'react';
+
+
 function ExpenseItem1(props){
+  console.log('ExpenseItem1 evaluated by React');
   const [title,setTitle] = useState(props.title);
+  const [amount,setAmount] = useState(props.amount);
     
-    const clickHandler=()=>{
+    const clickHandler1=()=>{
         setTitle('Updated!');
         
         console.log('clicked!');
     }
-
+    const clickHandler2=()=>{
+        setAmount(100);
+        
+        console.log('clicked!');
+    }
 
     return (
       
         <Card className='expense-item'>
         <ExpenseDate date={props.date}></ExpenseDate>
-        <ExpenseDetails title={title} amount={props.amount} location={props.location}></ExpenseDetails>
-        <button onClick={clickHandler}>Change title</button>
+        <ExpenseDetails title={title} amount={amount} location={props.location}></ExpenseDetails>
+        <button onClick={clickHandler1}>Change title</button>
+        <button onClick={clickHandler2}>Change amount</button>
         </Card>
   
     )
