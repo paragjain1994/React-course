e9b691a8eaa9f15a72e018fa2e7b8ba3a9c3304e
parag <paragjain528@gmail.com>
2022-10-27 14:46:27 +0530

    component ExpenseForm and NewExpense added

---
+++ src/components/NewExpense.js
@@ -0,0 +1,15 @@
+// invoked from App.js
+import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = () => {
+  return (
+    <div className='new-expense'>
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpense;
