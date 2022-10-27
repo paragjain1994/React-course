684cb6810731986e3d653b8bef7a61c4e675f432
parag <paragjain528@gmail.com>
2022-10-27 09:52:02 +0530

    buttom and clickHandler function added

--- src/components/ExpenseItem1.js
+++ src/components/ExpenseItem1.js
@@ -1,13 +1,22 @@
  import Card from './Card';
  import ExpenseDate from './ExpenseDate';
  import ExpenseDetails from './ExpenseDetails';
-import './ExpenseItem.css';
+ import './ExpenseItem.css';
 function ExpenseItem1(props){
+  
+    
+    const clickHandler=()=>{
+        
+        console.log('clicked!');
+    }
+
+
     return (
       
         <Card className='expense-item'>
         <ExpenseDate date={props.date}></ExpenseDate>
         <ExpenseDetails title={props.title} amount={props.amount} location={props.location}></ExpenseDetails>
+        <button onClick={clickHandler}>Change title</button>
         </Card>
   
     )
