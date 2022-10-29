13592e591ef88a92557c2dcb351410316000692a
parag <paragjain528@gmail.com>
2022-10-29 11:24:44 +0530

    dropdownYear is set to 2020 through useState in Expense.js component

--- src/components/ExpensesFilter.js
+++ src/components/ExpensesFilter.js
@@ -1,20 +1,20 @@
 import React from 'react';
-
 import './ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
-
   const dropdownChangeHandler = (event) =>{
     console.log(event.target.value);
-    props.dropdownYear(event.target.value);
+ 
+    
   }
-
+ 
+  
  
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select onChange={dropdownChangeHandler}>
+        <select onChange={dropdownChangeHandler} value={props.selected}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
