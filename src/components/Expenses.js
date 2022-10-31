ff4b3c5d08d450a518a0fba9f52a64a129f07b73
parag <paragjain528@gmail.com>
2022-10-31 20:12:33 +0530

    making dynamic with the help of map function instead of loop

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -7,29 +7,36 @@ import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props){
 const [filteredYear, setFilteredYear] = useState('2020');
-console.log('filtered yr before..'+ filteredYear);
-console.log(props.items.length);
+// console.log('filtered yr before..'+ filteredYear);
+console.log('length of array '+props.items.length);
 
-  const arrayOfObject =[];
-   for(let i=0;i<props.items.length;i++){
-     const items_=  <ExpenseItem1 title = {props.items[i].title} amount = {props.items[i].amount} date = {props.items[i].date} location = {props.items[i].location}></ExpenseItem1>;
-
-     arrayOfObject.push(items_);
-   }
+  // const arrayOfObject =[];
+  //  for(let i=0;i<props.items.length;i++){
+  //    const items_=  <ExpenseItem1 key={props.items[i].id} title = {props.items[i].title} amount = {props.items[i].amount} date = {props.items[i].date} location = {props.items[i].location}></ExpenseItem1>;
+  //    arrayOfObject.push(items_);
+  // }
 
    const dropdownYearHandler =(selectedYear)=>{
-    console.log(selectedYear);
+   // console.log(selectedYear);
     setFilteredYear(selectedYear);        // setFilteredYear function behaves asynchronously
-    console.log('filtered yr after..'+ filteredYear);
+   // console.log('filtered yr after..'+ filteredYear);
    }
 
     return(
-      <div>
+      
       <Card className="expenses">
       <ExpensesFilter dropdownYear ={dropdownYearHandler} selected ={filteredYear}></ExpensesFilter>
-      {arrayOfObject}
+      {/* {arrayOfObject} */}
+      {props.items.map((expense)=>(
+        <ExpenseItem1
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
       </Card>
-      </div>
+      
 )
    
     
