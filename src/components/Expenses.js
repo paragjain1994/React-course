9dc67e40e8bf0d83e3f4812ecdb6a33ebf18bd45
parag <paragjain528@gmail.com>
2022-11-03 10:42:03 +0530

    conditional rendering is seperated from Expenses.js component & added to ExpensesList.js component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,25 +1,20 @@
 // invoked from App.js comp
 import React , {useState} from 'react';
 import './Expenses.css';
-import ExpenseItem1 from './ExpenseItem1';
+import ExpensesList from './ExpensesList';
 import Card from './Card';
 import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props){
 const [filteredYear, setFilteredYear] = useState('2020');
-// console.log('filtered yr before..'+ filteredYear);
 console.log('length of array '+props.items.length);
 
-  // const arrayOfObject =[];
-  //  for(let i=0;i<props.items.length;i++){
-  //    const items_=  <ExpenseItem1 key={props.items[i].id} title = {props.items[i].title} amount = {props.items[i].amount} date = {props.items[i].date} location = {props.items[i].location}></ExpenseItem1>;
-  //    arrayOfObject.push(items_);
-  // }
+
 
    const dropdownYearHandler =(selectedYear)=>{
-   // console.log(selectedYear);
-    setFilteredYear(selectedYear);        // setFilteredYear function behaves asynchronously
-   // console.log('filtered yr after..'+ filteredYear);
+
+    setFilteredYear(selectedYear);     
+   
    }
 
    const filteredExpenses = props.items.filter(expense =>{
@@ -27,25 +22,24 @@ console.log('length of array '+props.items.length);
     return expense.date.getFullYear().toString() == filteredYear;
    })
 
-   let expensesContent = <p>No expenses found!</p>
+  //  let expensesContent = <p>No expenses found!</p>
 
-   if(filteredExpenses.length > 0){
-    expensesContent = filteredExpenses.map((expense)=>(
-      <ExpenseItem1
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-   }
+  //  if(filteredExpenses.length > 0){
+  //   expensesContent = filteredExpenses.map((expense)=>(
+  //     <ExpenseItem1
+  //       key={expense.id}
+  //       title={expense.title}
+  //       amount={expense.amount}
+  //       date={expense.date}
+  //     />
+  //   ))
+  //  }
 
     return(
        <Card className="expenses">
       <ExpensesFilter dropdownYear ={dropdownYearHandler} selected ={filteredYear}></ExpensesFilter>
-      {/* {arrayOfObject} */}
-
-      {expensesContent}
+  
+      <ExpensesList items={filteredExpenses}></ExpensesList>
       </Card>
       
 )
