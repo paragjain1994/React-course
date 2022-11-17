e68715a44da00ccb824fe0a7c3cbc3e5c2e01fd3
parag <paragjain528@gmail.com>
2022-11-18 00:43:26 +0530

    useEffect hook added for the filtered year

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,5 +1,5 @@
 // invoked from App.js comp
-import React , {useState} from 'react';
+import React , {useState , useEffect} from 'react';
 import './Expenses.css';
 import ExpensesList from './ExpensesList';
 import Card from './Card';
@@ -8,19 +8,27 @@ import ExpensesChart from './ExpensesChart';
 
 function Expenses(props){
 const [filteredYear, setFilteredYear] = useState('2020');
-console.log('length of array '+props.items.length);
+// console.log('length of array '+props.items.length);
+useEffect(()=>{
+   console.log('use effect')
+   if (localStorage.getItem('filteredYear')) {
+      setFilteredYear(localStorage.getItem('filteredYear'))
+   }
+},[])
+
 
 
 
    const dropdownYearHandler =(selectedYear)=>{
 
     setFilteredYear(selectedYear);     
+    localStorage.setItem('filteredYear',selectedYear);
    
    }
 
    const filteredExpenses = props.items.filter(expense =>{
-    console.log(expense,filteredYear);
-    return expense.date.getFullYear().toString() == filteredYear;
+   // console.log(expense,filteredYear);
+    return expense.date.getFullYear().toString() === filteredYear;
    })
 
   //  let expensesContent = <p>No expenses found!</p>
